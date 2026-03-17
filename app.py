ef6f4a16fb2d623a89d2ca1c57d70e3bbb35bbbf
Raviteja <praviteja02529@gmail.com>
2026-03-17 15:09:20 +0530

    add api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 from flask_cors import CORS
 from google import genai
 import os
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
